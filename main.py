d0fde2cb9819a25142ceb69b565974401c9d6cc3
viktor <viktog2210@gmail.com>
2024-04-10 18:58:56 +0200

    feat: Adding region of Intrest

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ object_detector = cv2.createBackgroundSubtractorMOG2()
 while True:
     ret, frame = cap.read()
 
-    mask = object_detector.apply(frame)
+    #Region of Interest
+    height, width, _ = frame.shape
+    print(height, width)
+    roi = frame[340: 720, 500: 800]    
 
+
+    mask = object_detector.apply(frame)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+
+    cv2.imshow("ROI", roi)
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
 
