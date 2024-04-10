01387967e9fb681abcdd7fc95c2240cedfa853c9
viktor <viktog2210@gmail.com>
2024-04-10 19:11:00 +0200

    feat: Adding tracking

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import cv2
+from tracker import *
+
+# Create tracker object
+tracker = EuclideanDistTracker()
 
 cap = cv2.VideoCapture("highway.mp4")
 
@@ -14,13 +18,20 @@ while True:
 
 
     mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > 100:
             #cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(roi, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            detections.append([x, y, w, h])
+
+    # Object tracking
+    boxes_ids = tracker.update(detections)
+    print(boxes_ids)
 
     cv2.imshow("ROI", roi)
     cv2.imshow("Frame", frame)
